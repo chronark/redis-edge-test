333cb999eb632766490d9434b20a3519d8678650
Andreas Thomas <dev@chronark.com>
2022-04-27 17:47:45 +0200

    initial commit

---
+++ netlify/edge-functions/hello.js
@@ -0,0 +1,8 @@
+import { Redis } from "https://deno.land/x/upstash_redis@v1.3.2-alpha.0/mod.ts";
+
+
+export default async () => {
+    const redis = new Redis.fromEnv()
+    const counter = await redis.incr("edge_counter")
+    return new Response(counter)
+}
